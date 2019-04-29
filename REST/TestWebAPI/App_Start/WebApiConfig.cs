bd4e3999f300632025cf174360769f3a3804c221
Zvonimir Vanjak <zvonimir.vanjak@gmail.com>
2019-04-29 18:10:34 +0200

    reorganization

--- REST/TestWebAPI/App_Start/WebApiConfig.cs
+++ REST/TestWebAPI/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace TestWebAPI
@@ -10,6 +11,7 @@ namespace TestWebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            config.EnableCors();
 
             // Web API routes
             config.MapHttpAttributeRoutes();
@@ -19,6 +21,9 @@ namespace TestWebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
--- REST/TestWebAPI/Controllers/ValuesController.cs
+++ REST/TestWebAPI/Controllers/ValuesController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Helpers;
 using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Results;
+using System.Web.Mvc;
 
 namespace TestWebAPI.Controllers
 {
+    public class Article
+    {
+        public string Title { get; set;  }
+        public string Content { get; set; }
+    }
     public class ValuesController : ApiController
     {
         // GET api/values
@@ -16,9 +25,15 @@ namespace TestWebAPI.Controllers
         }
 
         // GET api/values/5
+        [EnableCors(origins: "*", headers: "*", methods: "*")]
         public string Get(int id)
         {
-            return "value";
+            var newArticles = new List<Article> { new Article { Content = "Content", Title = "Title" } };
+            var newArticle = new Article{Content = "Content", Title = "Title" };
+            string yourJson = Newtonsoft.Json.JsonConvert.SerializeObject(newArticle);
+
+            //            return Json(newArticles.ToList());
+            return "Article " + id.ToString();
         }
 
         // POST api/values
